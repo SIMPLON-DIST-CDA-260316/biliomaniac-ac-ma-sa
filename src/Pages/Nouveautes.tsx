5bf78017a6917d1f626b4bcdc1cb7f2630dd4aa9
Anaïs <anais.cieslar@gmail.com>
2026-05-13 12:10:16 +0200

    feat: implement Nouveautes component

--- src/Pages/Nouveautes.tsx
+++ src/Pages/Nouveautes.tsx
@@ -1,7 +1,29 @@
+import {useEffect, useState} from 'react';
+import { fetchNouveautes } from '../shared/data/BookFetch.ts';
+import { BooksList } from '../shared/components/BooksList.tsx';
+import type { Book } from '../types/Book.ts';
+
 export default function Nouveautes() {
+  const [books, setBooks] = useState<Book[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    fetchNouveautes()
+      .then(setBooks)
+      .catch(() => setError('Impossible de charger les nouveautés.'))
+      .finally(() => setLoading(false));
+  }, []);
+
   return (
-    <section>
-      <h1>Nouveautés</h1>
+    <section className="flex flex-col items-center gap-10">
+      <h1 className="text-4xl md:text-5xl font-semibold text-(--text-h)">
+        Nouveautés
+      </h1>
+
+      {error && <p className="text-center text-red-500">{error}</p>}
+
+      <BooksList books={books} loading={loading} />
     </section>
   );
 }
