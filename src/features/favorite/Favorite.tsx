d2ec3883961c5f2971f6a5ddfab50e010d61ae89
Maxence Agullo <magullo35@gmail.com>
2026-05-13 11:55:09 +0200

    resolve conflicts caused by myself

--- src/features/favorite/Favorite.tsx
+++ src/features/favorite/Favorite.tsx
@@ -5,17 +5,6 @@ interface Props {
 
 const FavoriteButton = ({ isFavorite, onToggle }: Props) => {
 
-<<<<<<< HEAD
-    const handleClick = () => {
-        setIsFavorite((prev) => !prev);
-    };
-
-    return (
-        <img src={isFavorite ? "/images/starFull.svg" : "/images/starEmpty.svg"}
-        alt="" onClick={handleClick}
-        className="cursor-pointer w-8 h-8"/>
-    );
-=======
   return (
     <img
       src={
@@ -26,7 +15,6 @@ const FavoriteButton = ({ isFavorite, onToggle }: Props) => {
       className="cursor-pointer"
     />
   );
->>>>>>> bb4b082f8e1b2e4a6958e4432ed445e0b40700df
 };
 
 export default FavoriteButton;
